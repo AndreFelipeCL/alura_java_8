e6e43b027e27e12e02d5cdf8f89afa8f2d425966
André Felipe Camargo Leite <andrefelipecl@outlook.com>
2018-12-04 21:31:35 -0200

    Usando lambdas com Interfaces Funcionais; Usando inferência de método;

--- src/java8/OrdenaStrings.java
+++ src/java8/OrdenaStrings.java
@@ -1,42 +1,21 @@
 package java8;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class OrdenaStrings {
 
 	public static void main(String[] args) {
-		
+
 		List<String> palavras = new ArrayList<>();
 		palavras.add("André");
 		palavras.add("Felipe");
 		palavras.add("Camargo");
 		palavras.add("Leite");
-		
-		
-		palavras.sort(new ComparadorPorTamanho());
-		palavras.forEach(new ConsumidorDeStrings());
+
+		palavras.sort((o1, o2) -> Integer.compare(o1.length(), o2.length()));
+
+		palavras.forEach(t -> System.out.println(t));
 	}
 
-}
-
-
-class ConsumidorDeStrings implements Consumer<String> {
-
-	@Override
-	public void accept(String t) {
-		System.out.println(t);
-	}
-	
-}
-
-class ComparadorPorTamanho implements Comparator<String> {
-
-	@Override
-	public int compare(String o1, String o2) {
-		return Integer.compare(o1.length(), o2.length());
-	}
-	
 }
