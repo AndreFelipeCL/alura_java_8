63c6c89e8c724321ef0a80d17c8652bfbc8bfb3c
André Felipe Camargo Leite <andrefelipecl@outlook.com>
2018-12-04 20:59:27 -0200

    Primeiros passos: como era feito comparator no java1.7

---
+++ src/java8/OrdenaStrings.java
@@ -0,0 +1,34 @@
+package java8;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class OrdenaStrings {
+
+	public static void main(String[] args) {
+		
+		List<String> palavras = new ArrayList<>();
+		palavras.add("André");
+		palavras.add("Felipe");
+		palavras.add("Camargo");
+		palavras.add("Leite");
+		
+		Comparator<String> comparador = new ComparadorPorTamanho();
+		Collections.sort(palavras, comparador);
+		
+		System.out.println(palavras);
+		
+	}
+
+}
+
+class ComparadorPorTamanho implements Comparator<String> {
+
+	@Override
+	public int compare(String o1, String o2) {
+		return Integer.compare(o1.length(), o2.length());
+	}
+	
+}
