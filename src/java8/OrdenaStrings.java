e336551053a8f6d98f3963f23dc49679fee013f8
André Felipe Camargo Leite <andrefelipecl@outlook.com>
2018-12-04 22:36:32 -0200

    Usando lambdas com Tread

--- src/java8/OrdenaStrings.java
+++ src/java8/OrdenaStrings.java
@@ -1,33 +1,20 @@
 package java8;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class OrdenaStrings {
 
 	public static void main(String[] args) {
-		
+
 		List<String> palavras = new ArrayList<>();
 		palavras.add("André");
 		palavras.add("Felipe");
 		palavras.add("Camargo");
 		palavras.add("Leite");
-		
-		Comparator<String> comparador = new ComparadorPorTamanho();
-		
-		palavras.sort(comparador);
-		System.out.println(palavras);
-		
+
+		palavras.sort((o1, o2) -> Integer.compare(o1.length(), o2.length()));
+		palavras.forEach(t -> System.out.println(t));
 	}
 
-}
-
-class ComparadorPorTamanho implements Comparator<String> {
-
-	@Override
-	public int compare(String o1, String o2) {
-		return Integer.compare(o1.length(), o2.length());
-	}
-	
 }
